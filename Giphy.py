1a5c6afe99076366d8b7576d778389dbd424348e
Mikael Holmquist <mikael@storytel.com>
2019-04-28 23:08:02 +0200

    Remove empty lines

--- Giphy.py
+++ Giphy.py
@@ -4,7 +4,6 @@ import http
 import json
 import urllib
 
-
 class GiphyCommand(sublime_plugin.TextCommand):
 	
 	giphy_api_key = "" #fill in your api-key here
@@ -14,31 +13,19 @@ class GiphyCommand(sublime_plugin.TextCommand):
 		conn.request("GET", "/v1/gifs/random?api_key=" + self.giphy_api_key + "&tag=" + urllib.parse.quote(tag))
 		response = conn.getresponse().read().decode('utf-8')
 		j = json.loads(response)
-		
 		if not j["data"]:
 			return None
-
 		url = j["data"]["image_url"]
 		return url
 
 	def run(self, edit):
-		
 		for region in self.view.sel():
 			tag = self.view.substr(region).split("\n")[0]
-			
-			
 			url = self.get_gif_with_tag(tag)
-
 			if not url:
 				print("Could not find a gif with tag: \"" + tag + "\". Will fallback to a random gif.")
 				url = self.get_gif_with_tag("")
 				tag = "random"
-			
 			if not tag:
 				tag = "random"
-
 			self.view.replace(edit, region, "!["+tag+" gif](" + url + ")")
-		
-		
-
-
